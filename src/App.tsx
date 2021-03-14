f80d6130aeca60f2b821bf3e501c781bf19c4dbb
William Wold <wm@wmww.sh>
2021-03-13 16:40:12 -0800

    Add react-webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import Webcam from "react-webcam";
 import './App.css';
+
+class Viewer extends React.Component {
+  readonly webcamRef = React.createRef<Webcam>();
+  readonly canvasRef = React.createRef<HTMLCanvasElement>();
+
+  render() {
+    return <div>
+        <Webcam
+          ref={this.webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 720,
+            height: 500,
+          }}
+        />
+        <canvas
+          ref={this.canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 720,
+            height: 500,
+          }}
+        />
+    </div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Viewer />
     </div>
   );
 }
