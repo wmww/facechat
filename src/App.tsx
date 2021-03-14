88b406e227d0c30e2897a873a91bccea36a6d398
William Wold <wm@wmww.sh>
2021-03-13 17:36:07 -0800

    Add tensorflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import Webcam from "react-webcam";
 import './App.css';
+import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
 
-class Viewer extends React.Component {
+require('@tensorflow/tfjs-backend-webgl');
+
+/// See https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/readyState
+enum MediaReadyState {
+  Nothing = 0,
+  Metadata = 1,
+  CurrentData = 2,
+  FutureData = 3,
+  EnoughData = 4,
+}
+
+class Viewer extends React.Component<{}> {
   readonly webcamRef = React.createRef<Webcam>();
   readonly canvasRef = React.createRef<HTMLCanvasElement>();
+  private detector: faceLandmarksDetection.FaceLandmarksDetector | null = null;
+
+  constructor(props: {}) {
+    super(props);
+    faceLandmarksDetection.load(faceLandmarksDetection.SupportedPackages.mediapipeFacemesh).then(detector => {
+      this.detector = detector;
+    });
+    setInterval(() => {
+      this.detectFace();
+    }, 100);
+  }
+
+  async detectFace() {
+    const webcam = this.webcamRef.current;
+    const canvas = this.canvasRef.current;
+    if (
+      this.detector !== null &&
+      canvas !== null &&
+      webcam !== null &&
+      webcam.video !== null &&
+      webcam.video.readyState === MediaReadyState.EnoughData
+    ) {
+      const video = webcam.video;
+      video.width = video.videoWidth;
+      video.height = video.videoHeight;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const faces = await this.detector.estimateFaces({input: video});
+      console.log(faces);
+    }
+  }
 
   render() {
     return <div>
